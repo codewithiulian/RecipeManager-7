358ab3f6eec3f01f8ab200d075626a3bc4be994e
Iulian Oana <iulian.andreioana@gmail.com>
2021-10-02 09:38:19 +0100

    Added tutorial files

---
+++ after/DataAccess/Models/Recipe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataAccess.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "The title is too long, try a shorter one (500 characters limit)")]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(8000, ErrorMessage = "The description is too long, try a shorter one (8000 characters limit)")]
+        public string Description { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateUpdated { get; set; }
+    }
+}
---
+++ before/RecipeManager/Services/RecipeService.cs
@@ -0,0 +1,59 @@
+﻿using DataAccess.Data;
+using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeManager.Services
+{
+    public class RecipeService : IRecipeService
+    {
+        private readonly ApplicationDbContext _db;
+
+        public RecipeService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public Recipe Create(Recipe recipe)
+        {
+            recipe.DateCreated = DateTime.Now;
+            recipe.DateUpdated = DateTime.Now;
+
+            var newRecipe = _db.Recipes.Add(recipe);
+            _db.SaveChanges();
+
+            return newRecipe.Entity;
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Recipe Get(int id)
+        {
+            return _db.Recipes.Find(id);
+        }
+
+        public List<Recipe> List()
+        {
+            return _db.Recipes.OrderByDescending(o => o.DateUpdated).ToList();
+        }
+
+        public Recipe Update(Recipe recipe)
+        {
+            var dbRecipe = _db.Recipes.Find(recipe.Id);
+
+            if (dbRecipe != null)
+            {
+                dbRecipe = recipe;
+                dbRecipe.DateUpdated = DateTime.Now;
+
+                _db.SaveChanges();
+            }
+            return dbRecipe;
+        }
+    }
+}
